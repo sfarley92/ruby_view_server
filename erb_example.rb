be8c6b46ec4d6f155d2225e8f5db26b12a375f0b
Stewart Farley <SFarley92@yahoo.com>
2015-07-06 19:09:25 -0700

    homework 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,10 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new "The value of x is: #{x}"
 puts template.result(binding)
+
+y = {a: 6, b: 10, c: 25}
+y.each_key do |q|
+  puts "the key is #{q}"
+end
